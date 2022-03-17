7f7147382c46dc2232d40c7e517bc875ac2d7973
Kamiroo <kamil23.dominik@gmail.com>
2022-03-17 14:06:22 +0100

    Dodanie metod testowych srpawdzających poprawność działania tabeli

--- src/test/java/StudentTest.java
+++ src/test/java/StudentTest.java
@@ -12,4 +12,26 @@ public class StudentTest {
         assertEquals(true, student.addGrade(1));
     }
 
+    @Test
+    public void shouldReturnFalseWhenGradeIsLessThan1() {
+        assertEquals(false, student.addGrade(0));
+    }
+
+    @Test
+    public void shouldReturnFalseWhenGradeIsOver6() {
+        assertEquals(false, student.addGrade(7));
+    }
+
+    @Test
+    public void shouldReturnFalseWhenArrayIsFull() {
+        assertEquals(true, student.addGrade(6));
+        assertEquals(true, student.addGrade(1));
+        assertEquals(true, student.addGrade(3));
+        assertEquals(true, student.addGrade(5));
+        assertEquals(true, student.addGrade(6));
+        assertEquals(true, student.addGrade(2));
+        assertEquals(true, student.addGrade(1));
+        assertEquals(false, student.addGrade(3));
+    }
+
 }
